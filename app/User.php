0f6647dbae9d94e357fd9347bcbc449b21c89da5
Mihai Ionut Trofin <mihaitrof@gmail.com>
2017-05-03 17:25:40 +0300

    First migration

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 'FirstName','LastName',
     ];
 
     /**
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'id', 'Birthday', 'created_at','updated_at'
     ];
 }
